59ada945d53790b731b69cc9aafe6eaffe3f1163
Philipp Kant <ptckant@gmail.com>
2021-08-27 15:41:51 +0200

    obtain all comments of a specific submission

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import praw
 
 from secret import CLIENT_ID, CLIENT_SECRET, USER_AGENT, USERNAME, PASSWORD
 
+# https://praw.readthedocs.io/en/stable/getting_started/quick_start.html
+
+# create authorized reddit instance
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
@@ -9,3 +12,8 @@ reddit = praw.Reddit(
     username=USERNAME,
     password=PASSWORD,
 )
+
+# obtain all comments of a specific submission (may take a long time)
+submission = reddit.submission(id='pbgy9r')
+submission.comments.replace_more(limit=None)
+comments = submission.comments.list()
